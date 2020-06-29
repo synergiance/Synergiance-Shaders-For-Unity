f34cbe3c18369edcf42a22cebbda0f40eafd5de5
Synergiance <syn@synerfiles.info>
2020-06-29 19:09:49 -0400

    Update version string in AckToon

--- Assets/Resources/Shaders/Synergiance/Editor/BaseInspector.cs
+++ Assets/Resources/Shaders/Synergiance/Editor/BaseInspector.cs
@@ -8,7 +8,7 @@ using UnityEngine.Rendering;
 namespace Synergiance.Shaders.AckToon {
 	public class BaseInspector : SynInspectorBase {
 		
-		protected override string version { get { return "0.10"; } }
+		protected override string version { get { return "0.10b1"; } }
 
 		protected virtual bool hasEffects { get { return false; }}
 
