1a2003905fed965b3ab6ca58387038e4be82449f
Synergiance <syn@synerfiles.info>
2023-05-04 20:37:13 -0400

    Fix base inspector bugs
    - Fix an issue where new inspector was incompatible with certain editor versions, resulting in errors, and broken functionality.

--- Assets/Resources/Shaders/Synergiance/Editor/SynInspectorBase.cs
+++ Assets/Resources/Shaders/Synergiance/Editor/SynInspectorBase.cs
@@ -6,8 +6,6 @@ using UnityEngine.Rendering;
 
 namespace Synergiance.Shaders {
 	public class SynInspectorBase : ShaderGUI {
-		
-		
 		protected virtual string version => "1.0";
 
 		protected Material target;
@@ -24,19 +22,19 @@ namespace Synergiance.Shaders {
 				BoldFoldout(ref enabled, display == null ? MakeLabel(title) : display, function);
 			}
 		}
-		
+
 		protected static GUIContent staticLabel = new GUIContent();
 		#if UNITY_2017
 		protected static ColorPickerHDRConfig emissionConfig = new ColorPickerHDRConfig(0f, 99f, 1f / 99f, 3f);
 		#endif
-		
+
 		protected virtual bool hasGradient => false;
 		protected Dictionary<string, Gradient> gradients;
 		protected virtual bool hasString => false;
 		protected Dictionary<string, string> strings;
 		protected virtual bool hasFoldoutArray => false;
 		protected Dictionary<string, List<FoldoutItem> > FoldoutArrays;
-		
+
 		protected static void SetKeyword(Material m, string keyword, bool state) {
 			if (state) {
 				m.EnableKeyword(keyword);
@@ -44,7 +42,7 @@ namespace Synergiance.Shaders {
 				m.DisableKeyword(keyword);
 			}
 		}
-		
+
 		protected void SetKeyword(string keyword, bool state) {
 			foreach (Material m in editor.targets) {
 				SetKeyword(m, keyword, state);
@@ -54,33 +52,33 @@ namespace Synergiance.Shaders {
 		public static void WipeKeywords(Material m) {
 			foreach (string keyword in m.shaderKeywords) SetKeyword(m, keyword, false);
 		}
-		
+
 		protected static GUIContent MakeLabel(string text, string tooltip = null) {
 			GUIContent newLabel = new GUIContent(staticLabel);
 			newLabel.text = text;
 			newLabel.tooltip = tooltip;
 			return newLabel;
 		}
-		
+
 		protected static GUIContent MakeLabel(MaterialProperty property, string tooltip = null) {
 			GUIContent newLabel = new GUIContent(staticLabel);
 			newLabel.text = property.displayName;
 			newLabel.tooltip = tooltip;
 			return newLabel;
 		}
-		
-		static protected bool BoldFoldout(bool foldout, GUIContent content) {
+
+		static protected bool BoldFoldout(ref bool foldout, GUIContent content) {
 			GUIStyle BFstyle = new GUIStyle(EditorStyles.foldout);
 			BFstyle.fontStyle = FontStyle.Bold;
-			return EditorGUILayout.Foldout(foldout, content, BFstyle);
+			return foldout = EditorGUILayout.Foldout(foldout, content, BFstyle);
 		}
-		
+
 		static protected bool BoldFoldout(bool foldout, string content) {
-			return BoldFoldout(foldout, MakeLabel(content));
+			return BoldFoldout(ref foldout, MakeLabel(content));
 		}
 
 		static protected void BoldFoldout(ref bool foldout, GUIContent content, FoldoutFunc function) {
-			if (foldout = BoldFoldout(foldout, content)) {
+			if (BoldFoldout(ref foldout, content)) {
 				function();
 				EditorGUILayout.Space();
 			}
@@ -117,11 +115,11 @@ namespace Synergiance.Shaders {
 		protected void ShaderProperty(MaterialProperty prop, string display = null, string display2 = null) {
 			ShaderProperty(prop, (display != null ? MakeLabel(display, display2) : MakeLabel(prop)));
 		}
-		
+
 		protected void ShaderProperty(string enumName, string display = null, string display2 = null) {
 			ShaderProperty(FindProperty(enumName), display, display2);
 		}
-		
+
 		protected void ShowPropertyIfExists(string enumName, string display = null, string display2 = null) {
 			MaterialProperty property = FindProperty(enumName, false);
 			if (property != null) ShaderProperty(property, display, display2);
@@ -140,11 +138,11 @@ namespace Synergiance.Shaders {
 		protected void Vec2Prop(string label, MaterialProperty prop1, MaterialProperty prop2) {
 			Vec2Prop(MakeLabel(label), prop1, prop2);
 		}
-		
+
 		protected void Vec2IntProp(string label, MaterialProperty prop1, MaterialProperty prop2) {
 			Vec2IntProp(MakeLabel(label), prop1, prop2);
 		}
-		
+
 		protected void Vec2Prop(GUIContent label, MaterialProperty prop1, MaterialProperty prop2) {
 			EditorGUI.BeginChangeCheck();
 			EditorGUI.showMixedValue = prop1.hasMixedValue || prop2.hasMixedValue;
@@ -155,7 +153,7 @@ namespace Synergiance.Shaders {
 				prop2.floatValue = vec2.y;
 			}
 		}
-		
+
 		protected void Vec2IntProp(GUIContent label, MaterialProperty prop1, MaterialProperty prop2) {
 			EditorGUI.BeginChangeCheck();
 			EditorGUI.showMixedValue = prop1.hasMixedValue || prop2.hasMixedValue;
@@ -166,17 +164,17 @@ namespace Synergiance.Shaders {
 				prop2.floatValue = Mathf.Floor(vec2.y);
 			}
 		}
-		
+
 		protected void DoColorTexArea(string mapName, string colName, GUIContent label = null) {
 			MaterialProperty map = FindProperty(mapName);
 			MaterialProperty col = FindProperty(colName);
 			editor.TexturePropertySingleLine(label != null ? label : MakeLabel(map), map, col);
 		}
-		
+
 		protected void Vec3Prop(MaterialProperty prop, string display = null, string display2 = null) {
 			Vec3Prop(display == null ? MakeLabel(prop) : MakeLabel(display, display2), prop);
 		}
-		
+
 		protected void Vec3Prop(GUIContent label, MaterialProperty prop) {
 			EditorGUI.BeginChangeCheck();
 			EditorGUI.showMixedValue = prop.hasMixedValue;
@@ -184,7 +182,7 @@ namespace Synergiance.Shaders {
 			EditorGUI.showMixedValue = false;
 			if (EditorGUI.EndChangeCheck()) prop.vectorValue = new Vector4(vec3.x, vec3.y, vec3.z, prop.vectorValue.w);
 		}
-		
+
 		protected void HDRColorTextureProperty(GUIContent label, MaterialProperty textureProp, MaterialProperty colorProperty, bool showAlpha) {
 			#if UNITY_2017
 			editor.TexturePropertyWithHDRColor(label, textureProp, colorProperty, emissionConfig, showAlpha);
@@ -249,7 +247,7 @@ namespace Synergiance.Shaders {
 		protected string ReadStringFromMaterial(string prefix, int length) { return ""; }
 
 		protected void WriteStringToMaterial(string prefix, int length, string str) {}
-		
+
 		protected static bool HasClamp(MaterialProperty prop) {
 			if (prop.type != MaterialProperty.PropType.Texture) return false;
 			
@@ -260,14 +258,14 @@ namespace Synergiance.Shaders {
 
 			return false;
 		}
-		
+
 		/*
 		protected static void FixToonRampTexture(MaterialProperty prop) {
 			foreach (Material m in prop.targets)
 				m.GetTexture(prop.name).wrapMode = TextureWrapMode.Clamp;
 		}
 		*/
-		
+
 		protected void CheckForClamp(MaterialProperty prop) {
 			if (!HasClamp(prop)) {
 				EditorGUILayout.HelpBox("Set your texture's wrapping mode to clamp to get rid of glitches", MessageType.Warning);
@@ -278,7 +276,7 @@ namespace Synergiance.Shaders {
 				*/
 			}
 		}
-		
+
 		protected bool m_FirstTimeApply = true;
 		
 		public override void OnGUI(MaterialEditor editor, MaterialProperty[] properties) {
@@ -297,7 +295,7 @@ namespace Synergiance.Shaders {
 				m_FirstTimeApply = false;
 				InitializeInspector();
 			}
-			
+
 			EditorGUI.BeginChangeCheck();
 			DoMain();
 			if (EditorGUI.EndChangeCheck()) {
@@ -308,11 +306,11 @@ namespace Synergiance.Shaders {
 			EditorGUILayout.Space();
 			GUILayout.Label("Version: " + version);
 		}
-		
+
 		protected virtual void DoMain() {
 			GUILayout.Label("Please add DoMain() to your inspector!");
 		}
-		
+
 		protected virtual void MaterialChanged(Material material) {}
 
 		protected virtual void InitializeInspector() {}
